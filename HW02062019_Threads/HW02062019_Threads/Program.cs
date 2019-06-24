1b41efb8980b627e6671f4f817d4b26e187de971
ruslan261081 <45044693+ruslan261081@users.noreply.github.com>
2019-06-24 22:01:32 +0300

    Add files via upload

---
+++ HW02062019_Threads/HW02062019_Threads/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HW02062019_Threads
+{
+    class Program
+    {
+        private static void PrintMe()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(500);
+            }
+        }
+        private static void PrintHelloWorld()
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine("Hello World");
+                Thread.Sleep(250);
+            }
+        }
+        static void Main(string[] args)
+        {
+            ThreadExecutor executor = new ThreadExecutor();
+            for (int i = 0; i < 20; i++)
+            {
+                executor.Add(new Thread(PrintMe));
+                executor.Execute();
+                executor.Add(new Thread(PrintHelloWorld));
+                executor.Add(new Thread(() => Console.WriteLine("Hello from Lambda!!!")));
+            }
+            Console.WriteLine("Main has finished");
+        }
+    }
+}
---
+++ HW02062019_Threads/HW02062019_Threads/ThreadExecutor.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HW02062019_Threads
+{
+    class ThreadExecutor
+    {
+        Queue<Thread> _threadsQueue = new Queue<Thread>();
+        Thread _everySecondThread;
+
+        public void DoEverySecond()
+        {
+            while (true)
+            {
+                lock (this)
+                {
+                    if (_threadsQueue.Count > 0)
+                    {
+                        Execute();
+                    }
+                }
+                Thread.Sleep(1000);
+            }
+        }
+
+        public ThreadExecutor()
+        {
+            System.Timers.Timer timer = new System.Timers.Timer(1000);
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+        }
+
+
+        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            lock (this)
+            {
+                if (_threadsQueue.Count > 0)
+                {
+                    Execute();
+
+                }
+            }
+        }
+        public void ShutDown()
+        {
+            _everySecondThread.Abort();
+        }
+
+        public void Add(Thread thread)
+        {
+            lock (this)
+            {
+                Console.WriteLine($"new thread entered the queue");
+                if (thread != null)
+                {
+                    _threadsQueue.Enqueue(thread);
+                }
+            }
+        }
+        public void Execute()
+        {
+            lock (this)
+            {
+                while (_threadsQueue.Count > 0)
+                {
+
+                    Thread current = _threadsQueue.Dequeue();
+                    current.Start();
+                }
+            }
+
+        }
+    }
+}
